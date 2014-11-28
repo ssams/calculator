d0543a36a9ebc29ee8ef36b5d87f6d79bf809e4c
hp <herwig.plieger@gmail.com>
2014-11-28 08:38:47 +0100

    Function header

--- src/Function.cpp
+++ src/Function.cpp
@@ -5,27 +5,6 @@
 
 namespace calculator {
 
-Function::Function(Expression::PtrConst expression, std::set<std::string> params) :
-	expression(expression), params(params) {
-}
 
-Function::~Function() {
-
-}
-
-double Function::evaluate(std::vector<Expression::Ptr> paramValues) {
-	if(paramValues.size() != params.size()) {
-		throw std::runtime_error("invalid number of parameters");
-	}
-	std::map<std::string, Expression::Ptr> paramMap;
-
-	auto paramIter = params.begin();
-	for(auto paramValue : paramValues) {
-		paramMap[*paramIter] = paramValue;
-		paramIter++; // safe as both lists have same length
-	}
-
-	return expression->evaluate(paramMap);
-}
 
 } /* namespace calculator */
